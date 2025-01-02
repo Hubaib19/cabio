1e079371aaf59798ef5dbe0f93f721ab0c9eaf52
Hubaib Pullat <hubaib0019@gmail.com>
2025-01-02 23:34:58 +0530

    updated

--- your_project_name/lib/main.dart
+++ your_project_name/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:your_project_name/view/pages/homeScreen.dart';
-import 'package:your_project_name/view/pages/splash.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Splashscreen(),
+    return MaterialApp(
+      title: 'flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber
+      ),
+      home: const Homescreen(),
     );
+    
   }
 }
